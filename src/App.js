b3ce86461656aa482292e83f2eab49fc7ca60026
Mrinal Fouzdar <100467560+mrinalFouzdar@users.noreply.github.com>
2022-07-13 11:21:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   }, []);
   return (
     <div className="App">
-      mohaprobhu
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/from" element={<From />} />
